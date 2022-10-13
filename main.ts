729268fabd47a22bf248bfdc6cb2f298bac4dffb
pc20222012 <115674547+pc20222012@users.noreply.github.com>
2022-10-13 09:28:27 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,83 @@
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . . . #
+            . . . . .
+            # . . . #
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    while (true) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # . . .
+            # # . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # . .
+            # # # . .
+            # # # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            `)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(1000)
+    }
+})
+basic.forever(function () {
+	
+})
